ed11543ba7d9802c7a35f9ceb2fcd83295a9f49b
Wesley Wang <wesley.wang@qcif.edu.au>
2024-08-29 10:49:05 -0500

    feat: implement report table & display using  mock data

--- xero-balance-sheet-frontend/app/page.tsx
+++ xero-balance-sheet-frontend/app/page.tsx
@@ -1,7 +1,11 @@
+import ReportTable from "@/components/ReportTable";
+import { data } from "@/types";
+
 export default function Page() {
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold mb-8">Balance Sheet Report</h1>
+      <ReportTable report={data.Reports[0]} />
     </div>
   );
 }
